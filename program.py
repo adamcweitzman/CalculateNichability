49612571b503182d1709fa85c73580265e69dcee
Adam Cole Weitzman <adamcweitzman@gmail.com>
2017-03-17 18:22:26 -0500

    add test csv and works with csv reader

--- program.py
+++ program.py
@@ -1,7 +1,10 @@
 import sys
 import csv
 
-with open('../JungleScoutFiles/Search_Term_of_bat_houses.csv', 'rb') as csvfile:
-    reader = csv.DictReader(csvfile)
-for row in reader:
-    print("row")
+# path = '../JungleScoutFiles/Search_Term_of_bat_houses.csv'
+path = '../JungleScoutFiles/test.csv'
+
+with open('../JungleScoutFiles/Search_Term_of_bat_houses.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in reader:
+        print(', '.join(row))

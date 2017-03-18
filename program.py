759a94a359a9523202f65ffdbc56f9723dd968cd
Adam Cole Weitzman <adamcweitzman@gmail.com>
2017-03-17 21:41:54 -0500

    normalize downloaded files

--- program.py
+++ program.py
@@ -2,52 +2,47 @@ import sys
 import csv
 import math
 
+#path = '../JungleScoutFiles/Search_Term_of_bat_houses.csv'
+#path = '../JungleScoutFiles/Search_Term_of_shower_ curtains.csv'
+#path = '../JungleScoutFiles/test.csv'
+#path = '../JungleScoutFiles/Bedding.csv'
+path = '../JungleScoutFiles/TestFormat.csv'
 
 def roundup(x):
     return int(math.ceil(x / 100.0)) * 100
 
-path = '../JungleScoutFiles/Search_Term_of_bat_houses.csv'
-#path = '../JungleScoutFiles/Search_Term_of_shower_ curtains.csv'
-#path = '../JungleScoutFiles/test.csv'
 
-totalReviews = 0
-totalRevenue = 0
-numberOfRowsUnderFiftyReviews = 0
-count = 0
+def Main():
+    totalReviews = 0
+    totalRevenue = 0
+    numberOfRowsUnderFiftyReviews = 0
+    count = 0
 
-with open(path) as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        formattedRevenue = str(row['Est. Revenue']).replace(',', '').replace('$', '').replace('<', '')
-        formattedReview = str(row['# of Reviews']).replace(',', '')
-        if(formattedReview == "N.A."  or formattedRevenue == "N.A."):
-            continue
-        elif(int(formattedReview) <= 50):
-            print("hi")
-            revenue = int(formattedRevenue)
-            review = int(formattedReview)
-            totalRevenue += revenue
-            totalReviews += review
-            count += 1
+    with open(path) as csvfile:
+        for i in range(7):
+            csvfile.__next__()
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            formattedRevenue = str(row['Est. Revenue']).replace(',', '').replace('$', '').replace('<', '')
+            formattedReview = str(row['# of Reviews']).replace(',', '')
+            if(formattedReview == "N.A."  or formattedRevenue == "N.A."):
+                continue
+            elif(int(formattedReview) <= 50):
+                revenue = int(formattedRevenue)
+                review = int(formattedReview)
+                totalRevenue += revenue
+                totalReviews += review
+                count += 1
 
-AvgRevenue = totalRevenue / count
-AvgReview = totalReviews / count
-fullnessPercentage = (AvgReview / AvgRevenue) * 100
-score = str((500 - round(fullnessPercentage * 100)) * .2)
-print("Niche Score: " + score)
+    AvgRevenue = totalRevenue / count
+    AvgReview = totalReviews / count
+    fullnessPercentage = (AvgReview / AvgRevenue) * 100
+    score = str((500 - round(fullnessPercentage * 100)) * .2)
+    print("Niche Score: " + score)
+    input("press any key to continue...")
 
-# 3000 rev 10 reviews 33 BEST
-# 3000 rev 50 reviews 166 mid
-# 2000 rev 10 reviews mid 250
-# 3000 rev 30 reviews 100 mid
-# 1000 rev 45 reviews
-
-# 5200 and 5 .09 should be best
-# 19000 and 5 .02
-# 19000 and 250 1.3 should be worst
-
-
-#want low rank and low reviews
+if __name__ == "__main__":
+    Main()
 
 
 

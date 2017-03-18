50c903ab588ad277a3ad64f699f508e1e63ffa08
Adam Cole Weitzman <adamcweitzman@gmail.com>
2017-03-17 21:17:39 -0500

    now turns into a score based out of 100

--- program.py
+++ program.py
@@ -7,7 +7,8 @@ def roundup(x):
     return int(math.ceil(x / 100.0)) * 100
 
 path = '../JungleScoutFiles/Search_Term_of_bat_houses.csv'
-# path = '../JungleScoutFiles/test.csv'
+#path = '../JungleScoutFiles/Search_Term_of_shower_ curtains.csv'
+#path = '../JungleScoutFiles/test.csv'
 
 totalReviews = 0
 totalRevenue = 0
@@ -32,8 +33,14 @@ with open(path) as csvfile:
 AvgRevenue = totalRevenue / count
 AvgReview = totalReviews / count
 fullnessPercentage = (AvgReview / AvgRevenue) * 100
-print("Niche Score: " + str(round(fullnessPercentage * 100)))
+score = str((500 - round(fullnessPercentage * 100)) * .2)
+print("Niche Score: " + score)
 
+# 3000 rev 10 reviews 33 BEST
+# 3000 rev 50 reviews 166 mid
+# 2000 rev 10 reviews mid 250
+# 3000 rev 30 reviews 100 mid
+# 1000 rev 45 reviews
 
 # 5200 and 5 .09 should be best
 # 19000 and 5 .02
